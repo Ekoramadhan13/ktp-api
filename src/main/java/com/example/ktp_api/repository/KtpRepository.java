969c37cca9af1560c303d72baaae4ca3114779de
Ekoramadhan13 <enugraha411@gmail.com>
2026-03-09 19:30:19 +0700

    membuat file ApiResponse.java, KtpRepository.java dan mengisi code nya

--- src/main/java/com/example/ktp_api/repository/KtpRepository.java
+++ src/main/java/com/example/ktp_api/repository/KtpRepository.java
@@ -1,4 +1,19 @@
 package com.example.ktp_api.repository;
 
-public interface KtpRepository {
+import com.example.ktp_api.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+
+    // Cari data berdasarkan nomorKtp
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+
+    // Cek apakah nomorKtp sudah ada
+    boolean existsByNomorKtp(String nomorKtp);
+
+    // Cek nomorKtp sudah dipakai ID lain (validasi update)
+    boolean existsByNomorKtpAndIdNot(String nomorKtp, Integer id);
 }
